64e034979cfc282b9c90a81edeed7427128c17bc
Cláudio Diniz <cfpdiniz@gmail.com>
2017-01-18 20:31:23 +0000

    removing unnecessary import from DBImplicits

--- app/util/DBImplicits.scala
+++ app/util/DBImplicits.scala
@@ -2,14 +2,12 @@ package util
 
 import com.google.inject.Inject
 import play.api.db.slick.DatabaseConfigProvider
+import slick.dbio.DBIO
 import slick.driver.JdbcProfile
 
 import scala.concurrent.Future
 
 class DBImplicits @Inject()(dbConfigProvider: DatabaseConfigProvider) {
-  val driver: JdbcProfile = dbConfigProvider.get[JdbcProfile].driver
-  import driver.api._
-
   implicit def executeOperation[T](databaseOperation: DBIO[T]): Future[T] = {
     dbConfigProvider.get[JdbcProfile].db.run(databaseOperation)
   }
